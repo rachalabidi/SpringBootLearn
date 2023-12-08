2ef4cc9e4b30d52c2ea7e81ec32ffc34f3a527cd
rachalabidi <rashalabidi@gmail.com>
2023-12-08 18:32:41 +0000

    Naming path conventions & deprecating the v1 and working with the v2

--- src/main/java/com/coffee/learning/Customer/CustomerController.java
+++ src/main/java/com/coffee/learning/Customer/CustomerController.java
@@ -3,9 +3,12 @@ package com.coffee.learning.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
-@RequestMapping(path = "api/v1/customer")
+@RequestMapping(path = "api/v1/customers")
 @RestController
+@Deprecated
+
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -14,11 +17,14 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping(value = "all")
+    @GetMapping
     public List<Customer> GetCustomers() {
 
-        return customerService.GetCustomers();
+        return Arrays.asList(
+                new Customer( 1L , "rasha FROM V1 DEPRECATED " , "coffeV1")
+        );
     }
+
 
     @PostMapping
     void createNewCustomer(@RequestBody Customer customer) {
--- src/main/java/com/coffee/learning/Customer/CustomerControllerV2.java
+++ src/main/java/com/coffee/learning/Customer/CustomerControllerV2.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-@RequestMapping(path = "api/v2/customer")
+@RequestMapping(path = "api/v2/customers")
 @RestController
-@Deprecated
+
 public class CustomerControllerV2 {
     private final CustomerService customerService;
 
@@ -17,14 +18,21 @@ public class CustomerControllerV2 {
         this.customerService = customerService;
     }
 
-    @GetMapping(value = "all")
+    @GetMapping
     public List<Customer> GetCustomers() {
+        return customerService.GetCustomers();
 
-        return Arrays.asList(
-                new Customer( 1L , "rasha FROM V2" , "coffeV2")
-        );
     }
 
+    @GetMapping(path = "{customerId}")
+    public Customer GetCustomer(@PathVariable("customerId") Long id) {
+
+        return customerService.GetCustomers()
+                .stream()
+                .filter(customer -> Objects.equals(customer.getId(), id))
+                .findFirst()
+                .orElseThrow(()-> new IllegalStateException("customer not found "));
+    }
     @PostMapping
     void createNewCustomer(@RequestBody Customer customer) {
         System.out.println("Post request.....");
